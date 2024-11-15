3302c4a7f361e465183b7f9b372a84b813a447b4
Shweta SerpentCS <s.goltar.serpentcs@gmail.com>
2024-11-15 14:30:47 +0530

    [WIP] [18.0] web_groupby_expand: migrated module

--- web_groupby_expand/__manifest__.py
+++ web_groupby_expand/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Web GroupBy Expand",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Serpent Consulting Services Pvt. Ltd.",
     "maintainer": "Serpent Consulting Services Pvt. Ltd.",
     "website": "http://www.serpentcs.com",
@@ -12,6 +12,9 @@
     "depends": [
         "web",
     ],
+    "data": [
+
+    ],
     "assets": {
         "web.assets_backend": [
             "/web_groupby_expand/static/src/css/web_groups_expand.css",
@@ -19,6 +22,6 @@
             "/web_groupby_expand/static/src/xml/web_groups_expand.xml",
         ],
     },
-    "images": ["static/description/web_groupby_expand17.png"],
+    "images": ["static/description/groupexpand.png"],
     "installable": True,
 }
